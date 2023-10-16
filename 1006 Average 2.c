f1f5ec9469db7c7e6b715ff2cb2b63c7a5bda1e0
Samiha Binte Salauddin <samiha.salauddin@gmail.com>
2023-10-16 13:00:05 +0600

    Solved Average 2

---
+++ 1006 Average 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main () {
+    double a, b, c, average;
+    scanf ("%lf %lf %lf", &a, &b, &c);
+    average = (a*2 + b*3 + c*5)/10;
+    printf("MEDIA = %.1lf\n", average);
+
+    return 0;
+}
