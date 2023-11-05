6a654ecc46626cf91726cfc2a085a01c90533b8e
Samiha Binte Salauddin <samiha.salauddin@gmail.com>
2023-11-06 00:46:31 +0600

    Solved

---
+++ 1065 Even Between five Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main () {
+    int a, i, even = 0;
+    for (i = 0; i <5; i++){
+        scanf("%d", &a);
+        if (a%2==0)
+        even++;
+
+
+    }
+    printf("%d valores pares\n", even);
+
+    return 0;
+}
