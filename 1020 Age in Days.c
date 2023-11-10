3909a371828657e929dda1d8f3a90b8b44d0c11e
Samiha Binte Salauddin <samiha.salauddin@gmail.com>
2023-11-10 19:37:32 +0600

    Solevd

---
+++ 1020 Age in Days.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int age, years, months, days;
+
+    scanf("%d", &age);
+
+    years = age / 365;
+    months = (age % 365) / 30;
+    days = (age % 365) % 30;
+
+    printf("%d ano(s)\n", years);
+    printf("%d mes(es)\n", months);
+    printf("%d dia(s)\n", days);
+    return 0;
+}
+
+
+
+
+
